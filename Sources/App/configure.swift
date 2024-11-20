0f066c1cb05b1e167e0fbaa584b8ca831aa63d27
JeongDeokho <ekdj98@gmail.com>
2024-11-20 12:42:01 +0900

    hostname 변경

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,6 +4,7 @@ import Vapor
 
 public func configure(_ app: Application) async throws {
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    app.http.server.configuration.hostname = "0.0.0.0"
     app.migrations.add(CreateFireDispatch())
     try await app.autoMigrate()
     try routes(app)
